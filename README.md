6ad37adf4049c6a3b8cdcee6d2167b270fbc8aa2
jasonpereira96 <jasonpereira074@gmail.com>
2020-09-14 18:31:18 +0530

    changes

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 
 ## Instructions
 
-clone the repo
-
-Then, in the repo root folder run `npm install` to install the dependencies
+1. Clone the repo.
+2. Then, in the repo root folder run `npm install` to install the dependencies.
+3. Also, install DBBeaver: `sudo snap install dbeaver-ce`    https://dbeaver.io/download/
 
 
 You'll have to install the mysql DB seperately and configure it so that it can be accessed by a non root user :(
@@ -14,13 +14,7 @@ This was a tremendous pain point for me.
 You may have to create a user root with password root.
 
 
-Also, install DBBeaver:
-`sudo snap install dbeaver-ce`
-
-https://dbeaver.io/download/
-
-
-Once you have installed mysql, execute the .sql file in the database folder.
+Once you have installed mysql, execute the dump.sql file in the database folder.
 
 That will create the database for you.
 
@@ -28,3 +22,9 @@ That will create the database for you.
 In the `server.js` file, there are entries for hostname, username and password.
 These may change depending on which VM your DB is running on and what is the current username/password of the DB.
 
+
+### To run the server:
+1. Ensure that the mysql daemon process is up and running
+2. Ensure that the database has been created.
+3. Ensure that the DB hostname, username and password match those in the server.js file
+4. Then in the app_server folder, execute `node server.js`. This will start the server.
---
+++ database/dump.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 5.7.31, for Linux (x86_64)
+--
+-- Host: localhost    Database: Students
+-- ------------------------------------------------------
+-- Server version	5.7.31-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Students`
+--
+
+DROP TABLE IF EXISTS `Students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Students` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `branch` varchar(100) NOT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `phone` varchar(100) DEFAULT NULL,
+  `cgpa` float NOT NULL,
+  `degree` varchar(100) DEFAULT NULL,
+  `year` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Students`
+--
+
+LOCK TABLES `Students` WRITE;
+/*!40000 ALTER TABLE `Students` DISABLE KEYS */;
+INSERT INTO `Students` VALUES (1,'Daniel DSliva','Electronics','Borivali','9321524870',6.48,'BTECH','2'),(2,'Karan Mamaniya','Electronics','Mahim','1234345343',7.8,'BTECH','2'),(3,'Jason Pereira','Computer Science','Borivali','9769190949',7.1,'BE','4'),(4,'Sneha Joshi','Computer Science','Malad','9869815863',8.2,'BTECH','4'),(5,'Praveen Pacha','Electronics','Borivali','8897458125',7.7,'MTECH','1'),(6,'Rohit Sharma','Mechanical','Andheri','8896574932',8,'MTECH','2'),(7,'Reshma Samuel','Electronics','Kandivali','9784210520',7.5,'BTECH','2'),(8,'Kaustubh Mhatre','Mechanical','Borivali','9192138460',6.9,'MTECH','1'),(9,'Abhishek Kargutkar','Mechanical','Dahisar','9993652014',7.8,'BE','3'),(10,'Shilpi Sharma','Computer Science','Matunga','7894581250',9.2,'BE','3'),(11,'Karan Singh','Eletronics','Kandivali','9475612224',8.4,'BTECH','3');
+/*!40000 ALTER TABLE `Students` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-09-14 17:59:18
